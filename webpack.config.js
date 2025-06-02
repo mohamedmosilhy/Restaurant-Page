196a4380dd76b4defa6312a1d455e0e12681053d
mohamedmosilhy <93820559+mohamedmosilhy@users.noreply.github.com>
2025-06-03 01:56:42 +0300

    adding the webpack

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  devtool: "eval-source-map",
+  devServer: {
+    watchFiles: ["./src/index.js"],
+  },
+  plugins: [new HtmlWebpackPlugin({ template: "./src/template.html" })],
+  module: {
+    rules: [
+      { test: /\.css$/, use: ["style-loader", "css-loader"] },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|woff2?|eot|ttf|otf)$/i,
+        type: "asset/resource", // emits the file and returns a URL
+      },
+    ],
+  },
+};
